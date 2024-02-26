f731ad0c9a5185146c1daeeec56be11aa36a090d
Oscar Tomchuk <neron2006tom@gmail.com>
2024-02-26 19:56:04 +0200

    Add project files.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace HomeWork2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Tomagochi t = new Tomagochi();
+            ConsoleKeyInfo key;
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int delete = 0;
+            int da = 0;
+            do
+            {
+                t.Static();
+                Thread.Sleep(4000);
+                Console.Clear();
+                
+
+                do
+                {
+                    da = random.Next(1, 4);
+                }
+                while ( da == delete );
+
+                if (da == 1)
+                {
+                    t.starving -= 20;
+                    t.Eat();
+                    delete = 1;
+                    if(t.starving <= 0)
+                    {
+                        t.hp = -20;
+                    }
+                    
+                }
+                else if (da == 2)
+                {
+                    t.happiness -= 20;
+                    t.Walk();
+                    delete = 2;
+                    if (t.happiness <= 0)
+                    {
+                        t.hp = -20;
+                    }
+                }
+                else
+                {
+                    t.hp -= 20;
+                    t.Heal();
+                    delete = 3;
+                }
+                key = Console.ReadKey(true);
+                if (key.KeyChar == '1')
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    if (t.starving - 20 > 80)
+                    {
+                        t.starving = 100;
+                        Console.WriteLine("u feed ur Tomagochi");
+                    }
+                    else if(t.starving - 20 < 80)
+                    {
+                        t.starving += 20;
+                        Console.WriteLine("u feed ur Tomagochi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ur tomagochi doesn't want eat");
+                    }
+                    if (delete != 1)
+                    {
+                        t.hp -= 20;
+                    }
+
+
+                }
+                else if (key.KeyChar == '2')
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+
+                    if (t.starving - 20 > 80)
+                    {
+                        t.starving = 100;
+                        Console.WriteLine("u walk with ur Tomagochi");
+                    }
+                    else if (t.starving - 20 < 80)
+                    {
+                        t.starving += 20;
+                        Console.WriteLine("u walk with ur Tomagochi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ur tomagochi doesn't want walk with u right now");
+                    }
+                    if (delete != 2)
+                    {
+                        t.hp -= 20;
+                    }
+                }
+                Thread.Sleep(3000);
+                Console.Clear();
+
+            }
+            while (t.hp > 0);
+        }
+    }
+}
---
+++ HomeWork2/Tomagochi.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    public class Tomagochi
+    {
+        public int hp = 100;
+        public int starving = 100;
+        public int happiness = 100;
+
+
+        public void Static()
+        {
+            Console.WriteLine("  /|_/|  ");
+            Console.WriteLine(" ( o.o ) ");
+            Console.WriteLine(" > ^ < ");
+
+            Console.WriteLine();
+            Console.WriteLine("Status:");
+            Console.WriteLine($"hp {hp}");
+            Console.WriteLine($"starving {starving}");
+            Console.WriteLine($"happiness {happiness}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1. Feed");
+            Console.WriteLine("2. Walk");
+            Console.WriteLine("3. heal");
+            Console.WriteLine("4. Afk");
+            Console.WriteLine("5. Exit");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("   /|_/|         _________");
+            Console.WriteLine("  ( o.o )______ / Feed me \\");
+            Console.WriteLine("   > ^ <        \\ Me  !   /");
+            Console.WriteLine("                 ---------");
+
+            Console.WriteLine();
+            Console.WriteLine("Status:");
+            Console.WriteLine($"hp {hp}");
+            Console.WriteLine($"starving {starving}");
+            Console.WriteLine($"happiness {happiness}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1. Feed");
+            Console.WriteLine("2. Walk");
+            Console.WriteLine("3. heal");
+            Console.WriteLine("4. Afk");
+            Console.WriteLine("5. Exit");
+        }
+
+
+        public void Walk()
+        {
+            Console.WriteLine("   /|_/|         _________");
+            Console.WriteLine("  ( o.o )______ / Let's   \\");
+            Console.WriteLine("   > ^ <        \\ go for a/");
+            Console.WriteLine("                 \\ walk! /");
+            Console.WriteLine("                  -------");
+
+            Console.WriteLine();
+            Console.WriteLine("Status:");
+            Console.WriteLine($"hp {hp}");
+            Console.WriteLine($"starving {starving}");
+            Console.WriteLine($"happiness {happiness}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1. Feed");
+            Console.WriteLine("2. Walk");
+            Console.WriteLine("3. Heal");
+            Console.WriteLine("4. Afk");
+            Console.WriteLine("5. Exit");
+        }
+
+        public void Heal()
+        {
+            Console.WriteLine("   /|_/|         _________");
+            Console.WriteLine("  ( o.o )______ / I need  \\");
+            Console.WriteLine("   > ^ <        \\ healing /");
+            Console.WriteLine("                 ---------");
+
+            Console.WriteLine();
+            Console.WriteLine("Status:");
+            Console.WriteLine($"hp {hp}");
+            Console.WriteLine($"starving {starving}");
+            Console.WriteLine($"happiness {happiness}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1. Feed");
+            Console.WriteLine("2. Walk");
+            Console.WriteLine("3. Heal");
+            Console.WriteLine("4. Afk");
+            Console.WriteLine("5. Exit");
+        }
+
+        public void Thank()
+        {
+            Console.WriteLine("   /|_/|         _________");
+            Console.WriteLine("  ( o.o )______ / Thank   \\");
+            Console.WriteLine("   > ^ <        \\ you!    /");
+            Console.WriteLine("                 ---------");
+
+            Console.WriteLine();
+            Console.WriteLine("Status:");
+            Console.WriteLine($"hp {hp}");
+            Console.WriteLine($"starving {starving}");
+            Console.WriteLine($"happiness {happiness}");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1. Feed");
+            Console.WriteLine("2. Walk");
+            Console.WriteLine("3. Heal");
+            Console.WriteLine("4. Afk");
+            Console.WriteLine("5. Exit");
+        }
+
+
+
+
+
+
+
+
+    }
+}
